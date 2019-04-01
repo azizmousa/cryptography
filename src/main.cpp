06b986db0e3ad89c8c94cff5d5df1d74fabb1f37
Aziz Mousa <azizmousa1010@gmail.com>
2019-04-01 02:11:44 +0200

    monoalphabetic test

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,16 @@
 #include<cryptography/monoalphabetic.h>
 
 int main(int argc, char* argv[]){
+    std::string text = "Welcome To C++ Cipher Test";
+    // Caezar caezer(3);
+    Cryptography *p;
+    p = new MonoAlphabetic();
+    std::string cipherText = p->incrypt(text);
+    std::string plainText = p->decrypt(cipherText);
+    std::cout<< "cipherText: " << cipherText << std::endl;
+    std::cout<< "plaintext: " << plainText << std::endl;
+
+    delete p;
 
     return 0;
 }
